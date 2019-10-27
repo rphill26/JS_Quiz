5d4bcef826ee48fa277176b309d77d7cbfa3c133
Randall Phillips <rphill45@uncc.edu>
2019-10-27 18:30:10 -0400

    can't get my JSON stringify to work so this is as far as I can go

--- main.js
+++ main.js
@@ -3,28 +3,60 @@ var questionHeading = document.getElementById("question");
 var answersList = document.getElementById("answers");
 
 
+
 startBtn.addEventListener("click", function(event){
     document.getElementById("start-screen").classList.add("d-none");
     document.getElementById("qa-screen").classList.remove("d-none");
-
+    
+    
     questionHeading.textContent = questions[0].title;
     answersList.innerHTML = "";
-
+    
     
     var seconds = document.getElementById("timer").textContent;
     var timer = setInterval(function() {
         seconds--;
-     document.getElementById("timer").textContent = seconds;
+        document.getElementById("timer").textContent = seconds;
         if (seconds <= 0) clearInterval(timer);
-        stopQuiz();
     }, 1000);
-
+    
+    
+    
     var choices = questions[0].choices;
+    var answer = questions[0].answer;
     for (var i = 0; i < choices.length; i++){
         var newChoice = document.createElement("button");
         newChoice.textContent = choices[i];
         answersList.appendChild(newChoice);
-    }
+
+        if(newChoice.textContent === answer){
+            newChoice.setAttribute("class", "true");
+            console.log(newChoice.textContent);
+            console.log(newChoice.className);
+            console.log(typeof newChoice);
+        } else {
+            newChoice.setAttribute("class", "false");
+            console.log(newChoice.textContent);
+            console.log(newChoice.className);
+            console.log(typeof newChoice);
+        }
+
+        
+
+        newChoice.addEventListener("click", function(event){
+            if (newChoice.textContent === answer){
+                alert("correct");
+            } else {
+                alert("wrong");
+            }
+        });
+    }    
 });
+
+
+
+
+
+
 
 
--- questions.js
+++ questions.js
@@ -11,4 +11,3 @@ var questions = [
     },
     ///etc.
   ];
-  
