dc3bcd194b08be3b5e57890fe8c5df57613139b9
Randall Phillips <rphill45@uncc.edu>
2019-10-28 00:51:23 -0400

    added final list of questions & answers

--- questions.js
+++ questions.js
@@ -10,43 +10,43 @@ var questions = [
       answer: "parentheses"
     },
     {
-      title: "New Question # 3",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "Inside of what tag do you link your JavaScript file with?",
+      choices: ["<html>", "<script>", "<javascript>", "<js>"],
+      answer: "<script>"
     },
     {
-      title: "New Question # 4",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "You insert a JavaScript inside of _________ ",
+      choices: ["only the <body>", "only the <head>", "the <title>", "either <body> or <head>"],
+      answer: "either <body> or <head>"
     },
     {
-      title: "New Question # 5",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "How do you write 'Hello World' in an alert box?",
+      choices: ["msgBox('Hello World');", "msg('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
+      answer: "alert('Hello World');"
     },
     {
-      title: "New Question # 6",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "What is proper syntax for writing an IF statement?",
+      choices: ["if i = 5 then {}", "if (i==5){}", "if i == 5 then{}", "if i = 5{}"],
+      answer: "if (i==5){}"
     },
     {
-      title: "New Question # 7",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "What special characters does an array usually have?",
+      choices: ["()", "{}", "||", "[]"],
+      answer: "[]"
     },
     {
-      title: "New Question # 8",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "How do you delcare a JavaScript variable?",
+      choices: ["variable carName", "var carName", "v carName", "carName = var"],
+      answer: "var carName"
     },
     {
-      title: "New Question # 9",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "What will the following code return: Boolean(10 > 9)",
+      choices: ["false", "NaN", "true", "undefined"],
+      answer: "true"
     },
     {
-      title: "New Question # 10",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "How do you round the number 7.25, to the nearest integer?",
+      choices: ["Math.rnd(7.25)", "round(7.25)", "Math.round(7.25)", "rnd(7.25)"],
+      answer: "Math.round(7.25)"
     },
   ];
